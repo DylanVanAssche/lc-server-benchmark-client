3f3a4a61b2faa1b955454197655d6e38486f0634
Dylan Van Assche <dylan.vanassche@ugent.be>
2020-01-28 18:55:29 +0100

    Improve running scripts

--- run_benchmark_polling.sh
+++ run_benchmark_polling.sh
@@ -5,6 +5,6 @@ cd target/release
 for i in {1..250}
 do
    echo "Starting POLLING client $i with interval $INTERVAL ms"
-   ./lc-server-benchmark-client --mode polling --interval $INTERVAL
+   ./lc-server-benchmark-client --mode polling --interval $INTERVAL &
    sleep 0.1
 done
--- run_benchmark_pushing.sh
+++ run_benchmark_pushing.sh
@@ -4,6 +4,6 @@ cd target/release
 for i in {1..250}
 do
    echo "Starting PUSHING client $i"
-   ./lc-server-benchmark-client --mode pushing
+   ./lc-server-benchmark-client --mode pushing &
    sleep 0.1
 done
